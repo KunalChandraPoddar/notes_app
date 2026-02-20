08721eff855ed6dc72275cc4d2d08f0661d635b1
Kunal Chandra Poddar <kcpoddar.10052004@gmail.com>
2026-02-20 14:24:16 +0530

    Implemented the edit functionality.

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -6,13 +6,14 @@ import 'package:notes_app/data/constants/app_strings.dart';
 class DioClient {
   static void prepareDio() {
     Get.lazyPut<Dio>(
-        () => Dio(BaseOptions(
-              baseUrl: ApiConfig.baseUrl,
-              connectTimeout: const Duration(seconds: 10),
-              headers: {
-                AppStrings.contentType: AppStrings.applicationJson,
-              },
-            )),
-        fenix: true);
+      () => Dio(
+        BaseOptions(
+          baseUrl: ApiConfig.baseUrl,
+          connectTimeout: const Duration(seconds: 10),
+          headers: {AppStrings.contentType: AppStrings.applicationJson},
+        ),
+      ),
+      fenix: true,
+    );
   }
 }
